2ed7b041096befa30ea681e94925c2216b23d2d8
Sebastien Lelong <slelong@scripps.edu>
2018-10-17 10:55:13 -0700

    fix issue #13

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from config_hub import *
 
-DATA_ARCHIVE_ROOT = './data'
-DATA_PLUGIN_FOLDER = './plugins'
+DATA_ARCHIVE_ROOT = '/data/biothings_studio'
+DATA_PLUGIN_FOLDER = '/data/biothings_studio/plugins'
 DATA_SRC_SERVER = 'localhost'
 DATA_SRC_PORT = 27017
 DATA_SRC_DATABASE = 'biothings_src'
@@ -29,4 +29,4 @@ logger = setup_default_log("hub", LOG_FOLDER)
 RELEASE_PATH = os.path.join(DATA_ARCHIVE_ROOT,"release")
 CACHE_FOLDER = os.path.join(DATA_ARCHIVE_ROOT,'cache')
 
-RUN_DIR = './run'
+RUN_DIR = '/data/run'
