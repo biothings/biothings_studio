a225b617f89918eeb51c6bda81f0a39925f9f7fd
Sebastien Lelong <sebastien.lelong@gmail.com>
2018-11-13 19:41:25 +0000

    fix HubServer import

--- bin/hub.py
+++ bin/hub.py
@@ -6,7 +6,7 @@ biothings.config_for_app(config)
 logging.info("Hub DB backend: %s" % biothings.config.HUB_DB_BACKEND)
 logging.info("Hub database: %s" % biothings.config.DATA_HUB_DB_DATABASE)
 
-from biothings.utils.hub import HubServer
+from biothings.hub import HubServer
 import hub.dataload.sources
 server = HubServer(hub.dataload.sources,name="BioThings Studio")
 server.start()
