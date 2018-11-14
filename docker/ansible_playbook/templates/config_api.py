1122f5f470e6ca8e1ba446d3848f5e79d7f45db4
Sebastien Lelong <slelong@scripps.edu>
2018-11-14 13:02:08 -0800

    reorganize config files + new studio launcher using HubServer class

---
+++ docker/ansible_playbook/templates/config_api.py
@@ -0,0 +1,11 @@
+# SSH port for hub console
+HUB_SSH_PORT = 7022
+HUB_API_PORT = 7080
+
+# Hub name/icon url/version, for display purpose
+HUB_NAME = "Studio for MyGene"
+HUB_ICON = None # default one
+HUB_VERSION = "master"
+
+USE_RELOADER = True # so no need to restart hub when a datasource has changed
+
