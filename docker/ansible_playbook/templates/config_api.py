32ef562fa6070e7553fdbf6f101df51ae58271a4
Sebastien Lelong <slelong@scripps.edu>
2018-11-28 14:35:11 -0800

    api's icon for studio4*

--- docker/ansible_playbook/templates/config_api.py
+++ docker/ansible_playbook/templates/config_api.py
@@ -3,8 +3,8 @@ HUB_SSH_PORT = 7022
 HUB_API_PORT = 7080
 
 # Hub name/icon url/version, for display purpose
-HUB_NAME = "Studio for MyGene"
-HUB_ICON = None # default one
+HUB_NAME = "Studio for {{api_name}}"
+HUB_ICON = "http://biothings.io/static/img/{{ api_name | regex_replace('\.info','') }}-logo-shiny.svg"
 HUB_VERSION = "master"
 
 USE_RELOADER = True # so no need to restart hub when a datasource has changed
