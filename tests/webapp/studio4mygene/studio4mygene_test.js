1726988626052213d9abdb3b033ef52524437593
Sebastien Lelong <slelong@scripps.edu>
2020-02-13 08:40:40 -0800

    more studio4mygene tests

--- tests/webapp/studio4mygene/studio4mygene_test.js
+++ tests/webapp/studio4mygene/studio4mygene_test.js
@@ -1,6 +1,6 @@
 Feature('Studio4mygene');
 
-Scenario('Check few datasources', (I) => {
+/*Scenario('Check few datasources', (I) => {
   I.amOnPage('/');
   I.wait(1);
   I.seeTextEquals("27","i.database.icon + span")
@@ -51,17 +51,78 @@ Scenario("Dump/upload pharmgkb", (I) => {
   I.waitForText("clingen",2)
 });
 
-Scenario("Create build config", (I) => {
+/*Scenario("Create build config", (I) => {
   I.amOnPage("/")
   I.wait(1);
   I.click("Builds")
   I.click("Menu")
+  I.wait(1) // transition
+  I.dontSee("default") // our conf isn't there yet
   I.click("New configuration")
+  I.wait(1) // transition
+  I.see("Create/edit build configuration") // see the form
   I.fillField("#conf_name","default")
   I.fillField("#doc_type","gene")
   I.selectOption("#selected_sources","cpdb")
   I.selectOption("#selected_sources","pharmgkb")
   I.selectOption("#builders","hub.databuild.builder.MyGeneDataBuilder")
-  //I.click("OK") // cén't find, more than one
+  I.click("#newbuildconf_ok")
+  I.wait(1) // transition
+  I.dontSee("Create/edit build configuration") // form has closed
+  I.click("Menu")
+  I.see("default") // our conf isn't there yet
+});*/
+
+Scenario("Create build", (I) => {
+  I.amOnPage("/")
+  I.wait(1);
+  I.click("Builds")
+  I.click("Menu")
+  I.wait(1) // transition
+  I.see("default") // our conf isn't there yet
+  I.click("#config_default")
+  I.wait(1) // transition
+  I.see("Create new build")
+  I.click("#create_default")
+  I.wait(1) // transition
+  I.see("Enter a name for the merged data")
+  I.fillField("#target_name","test_build")
+  I.click("#newbuild_ok")
+  I.wait(1) // transition
+  I.dontSee("Enter a name for the merged data") // closed form
+  I.wait(1) // transition
+  I.see("test_build")
+  I.wait(10) // building
+  I.click("Logs")
+  I.see("metadata") // up to the end
+  I.click("test_build")
+  I.see("total_genes")
+  I.see("total_entrez_genes")
+  I.see("total_ensembl_genes")
+  I.see("total_ensembl_genes_mapped_to_entrez")
+  I.see("total_ensembl_only_genes")
+  I.see("total_species")
 });
 
+Scenario("Create full data release", (I) => {
+  I.amOnPage("/")
+  I.wait(1);
+  I.click("Builds")
+  I.wait(1)
+  I.see("test_build")
+  I.click("test_build")
+  I.waitForText("Releases")
+  I.click("Releases")
+  I.see("New release")
+  I.click("New release")
+  I.wait(1) // transition
+  I.selectOption({"name":"release_type"},"full")
+  I.fillField({"name":"index_name"},"test_index")
+  I.see("Select an indexer environment to create the index on")
+  I.selectOption({"name":"index_env"}, "test (localhost:9200)")
+  I.click("#newrelease_ok")
+  I.wait(1)
+  I.dontSee("Select an indexer environment to create the index on")
+  I.wait(5) // indexing
+  I.see("Index test_index was created on test environment")
+});
