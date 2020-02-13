063b33c6a5f1963d85a4f7c83d65a5bb88db7598
Sebastien Lelong <slelong@scripps.edu>
2020-02-13 09:21:48 -0800

    wait more for indexing ?

--- tests/webapp/studio4mygene/studio4mygene_test.js
+++ tests/webapp/studio4mygene/studio4mygene_test.js
@@ -123,6 +123,6 @@ Scenario("Create full data release", (I) => {
   I.click("#newrelease_ok")
   I.wait(1)
   I.dontSee("Select an indexer environment to create the index on")
-  I.wait(5) // indexing
+  I.wait(10) // indexing
   I.see("Index test_index was created on test environment")
 });
