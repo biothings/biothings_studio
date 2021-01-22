d69f1a73fb3237c8b59b663d25c9922f766b0785
Chunlei Wu <anewgene@yahoo.com>
2021-01-22 08:35:44 -0800

    add studio webapp folder

---
+++ webapp/src/Utils.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        html2json: function(html) {
+            // remove html tags to get a clean json doc
+            html = html ? html : "{}";
+            html = html.replace(/<.*?>/g,""); // non-greedy to keep content between tags
+            try {
+                return JSON.parse(html);
+            } catch(err) {
+                console.log(`Error parsing mapping: ${err}`);
+                console.log(html);
+                throw err;
+            }
+        },
+    },
+}
+
+</script>
