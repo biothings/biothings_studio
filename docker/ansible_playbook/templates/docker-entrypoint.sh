80e3bccd5c62ed8eb2d2bec4eb014b5b870f3511
Chunlei Wu <anewgene@yahoo.com>
2021-05-28 21:35:07 -0700

    use bundled jvm to start cerebro if available

--- docker/ansible_playbook/templates/docker-entrypoint.sh
+++ docker/ansible_playbook/templates/docker-entrypoint.sh
@@ -43,7 +43,18 @@ do
 done
 
 # start Cerebro under elasticsearch user
-start-stop-daemon --start -c elasticsearch -b --exec /usr/local/cerebro/bin/cerebro
+if [[ -f "/usr/share/elasticsearch/jdk/bin/java" ]]; then
+# use bundled jvm if available, $bundled_jvm variable is checked in cerebro start script
+# the JAVA_OPTS below prevents a java error when starting cerebro with ES7's bundled jvm
+#   see more at https://github.com/lmenezes/cerebro/issues/514
+  start-stop-daemon --start -c elasticsearch -b --exec \
+    /usr/bin/env bundled_jvm="/usr/share/elasticsearch/jdk/" \
+                 JAVA_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED" \
+    /usr/local/cerebro/bin/cerebro
+else
+  start-stop-daemon --start -c elasticsearch -b --exec \
+    /usr/local/cerebro/bin/cerebro
+fi
 netstat -tnlp | grep 9000
 
 ret=$?
