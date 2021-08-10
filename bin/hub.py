f8fee34fb98899580e4c0b2270875b6533029c9a
Jerry <xzhou@scripps.edu>
2021-08-10 13:34:01 -0700

    add remote debug instruction

--- bin/hub.py
+++ bin/hub.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import os, logging
+import biothings
+import config
+import hub.dataload.sources
+from biothings.hub import HubServer
+from biothings.utils.version import set_versions
+import os
+import logging
 # shut some mouths
 logging.getLogger("botocore").setLevel(logging.ERROR)
 logging.getLogger("boto3").setLevel(logging.ERROR)
@@ -8,25 +14,31 @@ logging.getLogger("s3transfer").setLevel(logging.ERROR)
 logging.getLogger("urllib3").setLevel(logging.ERROR)
 
 
-import config, biothings
-from biothings.utils.version import set_versions
-app_folder,_src = os.path.split(os.path.split(os.path.abspath(__file__))[0])
-set_versions(config,app_folder)
+app_folder, _src = os.path.split(os.path.split(os.path.abspath(__file__))[0])
+set_versions(config, app_folder)
 biothings.config_for_app(config)
 logging = config.logger
 
-from biothings.hub import HubServer
-import hub.dataload.sources
-server = HubServer(hub.dataload.sources,name="BioThings Studio")
+server = HubServer(hub.dataload.sources, name="BioThings Studio")
 
+# import ptvsd
+
+# Allow other computers to attach to ptvsd at this IP address and port.
+# ptvsd.enable_attach(address=('1.2.3.4', 3000), redirect_output=True)
+
+# Pause the program until a remote debugger is attached
+# ptvsd.wait_for_attach()
 
 if __name__ == "__main__":
     # vanilla or as a launcher of an API
     from optparse import OptionParser
-    import os, sys, glob, re
+    import os
+    import sys
+    import glob
+    import re
 
     parser = OptionParser()
-    parser.add_option("-a","--api-folder",help="API folder to run", dest="api_folder")
+    parser.add_option("-a", "--api-folder", help="API folder to run", dest="api_folder")
     (options, args) = parser.parse_args()
     if options.api_folder:
         api_folder = os.path.abspath(options.api_folder)
@@ -35,7 +47,7 @@ if __name__ == "__main__":
         # assuming a bin/hub.py module in this folder
         os.chdir(api_folder)
         assert "bin" in os.listdir(), "Can't find 'bin' folder containing hub.py"
-        scripts = glob.glob(os.path.join("bin","*.py"))
+        scripts = glob.glob(os.path.join("bin", "*.py"))
         print(scripts)
         startup = None
         if len(scripts) == 1:
@@ -44,31 +56,31 @@ if __name__ == "__main__":
             if "bin/hub.py" in scripts:
                 startup = scripts[scripts.index("bin/hub.py")]
             else:
-                logging.error("Found more than one startup scripts, none of them named hub.py, " + \
-                        "don't know which to choose: %s" % scripts)
+                logging.error("Found more than one startup scripts, none of them named hub.py, " +
+                              "don't know which to choose: %s" % scripts)
                 sys.exit(1)
         logging.info("Found startup script '%s'" % startup)
-        strmod = re.sub(".py$","",startup).replace("/",".")
+        strmod = re.sub(".py$", "", startup).replace("/", ".")
         import importlib
         mod = importlib.import_module(strmod)
         from biothings.hub import HubServer
         # try to locate a hub server instance
         for name in dir(mod):
-            server = getattr(mod,name)
-            if issubclass(server.__class__,HubServer):
+            server = getattr(mod, name)
+            if issubclass(server.__class__, HubServer):
                 logging.info("Found hub server: %s" % server)
                 # replace sources, dynamic discovery
                 if os.path.exists("hub/dataload/sources"):
                     server.source_list = "hub/dataload/sources"
                     logging.info("Auto-discovering sources in 'hub/dataload/sources'")
                 else:
-                    logging.warning("Couldn't locate sources folder (expecting 'hub/dataload/sources'), keep those defined by the API")
+                    logging.warning(
+                        "Couldn't locate sources folder (expecting 'hub/dataload/sources'), keep those defined by the API")
                 break
     else:
         logging.info("Runing vanilla studio")
 
     logging.info("Hub DB backend: %s" % biothings.config.HUB_DB_BACKEND)
     logging.info("Hub database: %s" % biothings.config.DATA_HUB_DB_DATABASE)
-
 
     server.start()
